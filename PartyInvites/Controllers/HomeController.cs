78bc17ebee06e3832008d314665b74059f2def7d
Yodle_94 <sj_artist@naver.com>
2023-03-08 22:26:31 +0900

    초기 커밋 실수

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            ViewBag.Greeting = DateTime.Now.Hour < 12 ? "Morning" : "Afternoon";
+            return View();
+        }
+
+    }
+}
